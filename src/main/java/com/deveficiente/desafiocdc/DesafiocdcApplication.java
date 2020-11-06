daab5e38a5e34cfdffc765e7e92f9a53f79cea4b
Nayra Oliveira <nayrabiaoliveira@gmail.com>
2020-11-05 23:47:09 -0300

    Criado projeto com as configurações iniciais e adicionado dependências
    Dependências adicionadas: spring-boot-starter-data-jpa,
    spring-boot-starter-validation, spring-boot-starter-web,
    spring-boot-devtools, com.h2database e spring-boot-starter-test

---
+++ src/main/java/com/deveficiente/desafiocdc/DesafiocdcApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiocdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveficiente/desafiocdc/DesafiocdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.desafiocdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
